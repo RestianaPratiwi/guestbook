5f835c6377e9291b3f9ce131984386b4c5cb3af3
Restiana <restianapratiwi324@gmail.com>
2024-09-13 09:06:00 +0700

    membuat routing, migration, dan model

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,15 @@ Auth::routes([
     'verify' => false,
 ]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group([
+    'middleware'=> ['auth'],
+    'prefix' => 'admin', //admin/tamu
+    'as' => 'admin.'
+], function(){
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index'); // guestbook.test/admin/
+
+    //guestbook.test/admin/dashboard -> route('admin.dashboard')
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+});
